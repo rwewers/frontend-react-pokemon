550b6623e1c1760821b3fb269407797735299dc7
rwewers <remco@wewers.nl>
2020-12-14 18:03:15 +0100

    so far

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [pokemons, setPokemons] = useState(null);
+    async function fetchdata(){
+
+        try{
+            const result = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
+            console.log(result);
+            setPokemons(result.data.results);
+        } catch(e){
+            console.error(e);
+        }
+    }
+    return(
+        <div>
+            <button type="button" onClick={fetchdata}>
+                Haal de pokemons op!
+            </button>
+            <ul>
+
+                {pokemons && pokemons.map((pokemon)=>{
+
+                    return <li>{pokemon.name}</li>
+
+                } ) }
+            </ul>
+
+        </div>
+    );
 }
+
 
 export default App;
